17f312eba56a25aa2faa158370ebde6afe135fc2
jmichelin <jmichelinphone@gmail.com>
2017-02-04 16:17:28 -0500

    feat(interviewQuestions): delete interview question

--- src/components/InterviewList.js
+++ src/components/InterviewList.js
@@ -2,45 +2,51 @@
  * Created by jmichelin on 12/28/16.
  */
 import React from 'react'
-import { Panel, Button } from 'react-bootstrap'
+import {Panel, Button} from 'react-bootstrap'
 import InterviewQuestionList from './InterviewQuestions/List'
 import InterviewQuestionCreate from './InterviewQuestions/Create'
 import InterviewQuestionUpdate from './InterviewQuestions/Update'
+import axios from 'axios'
 
 class Interview extends React.Component {
   constructor() {
     super();
     this.state = {
       newInterviewQuestion: {
-      "genre": '',
-      "title": '',
-      "timeToAnswer": 1,
-      "questionText": '',
-      "answerText": '',
-      "status": ''
-    },
-      interviewQuestionList: [1,2,3],
+        "genre": '',
+        "title": '',
+        "timeToAnswer": 1,
+        "questionText": '',
+        "answerText": '',
+        "status": ''
+      },
+      interviewQuestionList: [
+        {
+          "genre": 'Please Wait While Loading',
+          "title": '',
+          "timeToAnswer": 1,
+          "questionText": '',
+          "answerText": '',
+          "status": ''
+        }
+      ],
       open: false
     }
     this.handleAddQuestion = this.handleAddQuestion.bind(this);
+    this.handleRemoveQuestion = this.handleRemoveQuestion.bind(this);
   }
 
   componentDidMount() {
-    //get interview questions here
     //console.log('InterviewList.js => Did Mount');
-    let fetchUrl = 'https://infinite-waters-52055.herokuapp.com/interview/questions/list'
-    fetch(fetchUrl)
-      .then(
-        (response) => {
-          return response.json()
-        }
-      )
-      .then(
-        (interviewQuestionList: []) => {
-          //console.log('+++interviewQuestions/List.js ---> interviewQuestionList => ', interviewQuestionList);
+    let status = this.props.params.status;
+    let fetchUrl = 'https://infinite-waters-52055.herokuapp.com/interview/questions/list/'+status;
+    axios.get(fetchUrl)
+      .then((returnedInterviewQuestionList: []) => {
+          let interviewQuestionList = returnedInterviewQuestionList.data;
           return this.setState({interviewQuestionList})
         }
-      )
+      );
+
   }
 
   componentWillUnmount() {
@@ -48,62 +54,102 @@ class Interview extends React.Component {
   }
 
 
-/*
-  componentWillMount() {
-    console.log('InterviewList.js => Will Mount')
-  }
+  /*
+   componentWillMount() {
+   console.log('InterviewList.js => Will Mount')
+   }
 
-  componentWillReceiveProps() {
-    console.log('InterviewList.js => Will Receive Props');
-  }
+   componentWillReceiveProps() {
+   console.log('InterviewList.js => Will Receive Props');
+   }
 
-  shouldComponentUpdate() {
-    console.log('InterviewList.js => Should Component Update');
-    return true;
-  }
+   shouldComponentUpdate() {
+   console.log('InterviewList.js => Should Component Update');
+   return true;
+   }
 
-  componentWillUpdate() {
-    console.log('InterviewList.js => Will Update');
-  }
+   componentWillUpdate() {
+   console.log('InterviewList.js => Will Update');
+   }
 
-  componentDidUpdate() {
-    console.log('InterviewList.js => Did Update');
-  }
-  */
+   componentDidUpdate() {
+   console.log('InterviewList.js => Did Update');
+   }
+   */
 
   handleAddQuestion(question) {
+    //console.log('handleAddQuestion this.state.interviewQuestionList -> ', this.state.interviewQuestionList);
     event.preventDefault();
+    let postUrl = 'https://infinite-waters-52055.herokuapp.com/interview/question/add';
+    //let postUrl = 'http://localhost:7337/interview/question/add';
     let data = question;
-    console.log('+++ handleAddQuestion data => ', data);
-    fetch('https://infinite-waters-52055.herokuapp.com/interview/question/add', {
+    //console.log('interviewList -> handleAddQuestion -> data => ', data);
+    axios({
       method: 'post',
-      headers: {
-        "Content-Type": "application/json; charset=UTF-8;"
-      },
-      mode: 'no-cors',
-      body: JSON.stringify(data)
+      url: postUrl,
+      data: {
+        "genre": data.genre,
+        "title": data.title,
+        "timeToAnswer": data.timeToAnswer,
+        "questionText": data.questionText,
+        "answerText": data.answerText,
+        "status": "active"
+      }
     })
       .then((response) => {
-        console.log('response.status ', response.status);
-        if(response.ok){
-          response.json().then((interviewQuestion)=>{
-            console.log('interviewQuestion=>  ', interviewQuestion);
-          })
-        } else {
-          console.log('response not okay: ', response);
-          //this.state.push
-        }
-        console.log('this.state', this.state);
-        console.log('this.props', this.props);
-        //this.setState(data);
+        //console.log('handleAddQuestion axios.then this.state.interviewQuestionList -> ', this.state.interviewQuestionList);
+        //console.log('interviewList - response.data => ', response.data);
+        data._id = response.data.id;
+        this.setState(previousState => {
+          return {
+            interviewQuestionList: [...previousState.interviewQuestionList, data]
+          }});
       })
-      .catch((e) => console.log('There has been a problem with your fetch operation: ' + e.message));
-    this.setState(previousState => ({
-      interviewQuestionList: [...previousState.interviewQuestionList, data]
-    }));
+      .catch((error) => {
+        console.error('interviewList - error => ', error);
+      });
+    //console.log('this.state.interviewQuestionList -> ', this.state.interviewQuestionList);
+    //console.log('+++ handleAddQuestion data => ', data);
   }
 
+  handleRemoveQuestion (questionID) {
+    event.preventDefault();
+    console.log('value of question in InterviewList.js => ', questionID);
+    let question = {
+      "id": questionID,
+      "status": "inactive"
+    };
+    console.log('question => ', question);
+    //TODO remove question axios call
+    let postUrl = 'https://infinite-waters-52055.herokuapp.com/interview/question/update';
+    //let postUrl = 'http://localhost:7337/interview/question/update';
+    axios({
+      method: 'post',
+      url: postUrl,
+      data: question
+    })
+      .then((response) => {
+        //console.log('handleAddQuestion axios.then this.state.interviewQuestionList -> ', this.state.interviewQuestionList);
+        console.log('line 133: interviewList->handleRemoveQuestion -> response.data => ', response.data);
+        console.log('this.state.interviewQuestionList=> ', this.state.interviewQuestionList);
+        let removedInterviewQuestionIndex = this.state.interviewQuestionList.findIndex(function(element){
+          //console.log(element._id,' = ', questionID);
+          return element._id === questionID;
+        });
+        this.state.interviewQuestionList.splice(removedInterviewQuestionIndex, 1);
+        let newInterviewQuestionList = this.state.interviewQuestionList;
+        console.log('newInterviewQuestionList => ', newInterviewQuestionList);
+        console.log('this.state.interviewQuestionList=> ', this.state.interviewQuestionList);
+        //TODO remove from state
+        this.setState({
+            interviewQuestionList: newInterviewQuestionList
+          });
+      })
+      .catch((error) => {
+        console.error('interviewList - error => ', error);
+      });
 
+  }
 
 
   render() {
@@ -114,7 +160,7 @@ class Interview extends React.Component {
       <div>
         <div className="row">
           <div className="col-md-12">
-            <Button bsStyle="primary" onClick={ ()=> this.setState({ open: !this.state.open })}>
+            <Button bsStyle="primary" onClick={ () => this.setState({open: !this.state.open})}>
               Interview Question Form
             </Button>
             <Panel className="App-main" collapsible expanded={this.state.open}>
@@ -131,6 +177,7 @@ class Interview extends React.Component {
             <InterviewQuestionList
               status={this.props.params.status}
               interviewQuestionList={this.state.interviewQuestionList}
+              removeQuestion={this.handleRemoveQuestion}
             />
           </div>
         </div>
--- src/components/InterviewQuestions/List.js
+++ src/components/InterviewQuestions/List.js
@@ -1,16 +1,17 @@
 /**
  * Created by jmichelin on 12/28/16.
- * TODO Fetch and display questions
  */
 import React from 'react';
-import { Accordion, Panel } from 'react-bootstrap'
+import { Panel } from 'react-bootstrap'
+import $ from 'jquery'
 
 class InterviewQuestionList extends React.Component {
-  constructor () {
-    super()
+  constructor (props) {
+    super(props);
     this.state = {
-      interviewQuestionList: []
-    }
+      open: false
+    };
+    this.handleRemoveQuestion = this.handleRemoveQuestion.bind(this);
   }
 
   shouldComponentUpdate() {
@@ -39,39 +40,35 @@ class InterviewQuestionList extends React.Component {
 
   // componentWillMount () {
   //   console.log('InterviewQuestions/List.js => Will Mount')
-  //   let fetchUrl = 'https://infinite-waters-52055.herokuapp.com/interview/questions/list'
-  //   fetch(fetchUrl)
-  //     .then(
-  //       (response) => {
-  //         return response.json()
-  //       }
-  //     )
-  //     .then(
-  //       (interviewQuestionList: []) => {
-  //         //console.log('+++interviewQuestions/List.js ---> interviewQuestionList => ', interviewQuestionList);
-  //         return this.setState({interviewQuestionList})
-  //       }
-  //     )
   // }
 
   filter (e) {
     this.setState({filter: e.target.value})
   }
 
+  handleRemoveQuestion (event) {
+    event.preventDefault();
+    let questionID = event.target.value;
+    console.log('value of questionID in List.js => ', questionID);
+    this.props.removeQuestion(questionID);
+  }
+
+
   render () {
     //console.log('List', this.props.interviewQuestionList);
 
-    let interviewQuestionList = this.props.interviewQuestionList
+    let interviewQuestionList = this.props.interviewQuestionList || [];
 
     if(this.state.filter) {
-      interviewQuestionList = interviewQuestionList.filter(interviewQuestion =>
-        interviewQuestion.genre.toLowerCase()
-          .includes(this.state.filter.toLowerCase()))
+      interviewQuestionList = interviewQuestionList.filter(interviewQuestion => {
+        //console.log('interviewQuestion', interviewQuestion);
+        return interviewQuestion.genre.toLowerCase()
+          .includes(this.state.filter.toLowerCase())})
     }
 
     let interviewQuestions = interviewQuestionList.map((interviewQuestion, index) => {
         let curKey = interviewQuestion._id||index;
-        return <InterviewQuestion interview={interviewQuestion} key={curKey}/>
+        return <InterviewQuestion interview={interviewQuestion} key={curKey} removeQuestion={this.handleRemoveQuestion.bind(this)}/>
     });
 
     //console.log('interviewQuestions length and value => \n', interviewQuestions.length, '\n', interviewQuestions);
@@ -84,10 +81,6 @@ class InterviewQuestionList extends React.Component {
           <input className="form-control" style={{marginBottom: 10}} type="text" placeholder="Start typing to filter" onChange={this.filter.bind(this)}/>
         </div>
         <div>{interviewQuestions}</div>
-        {/*{interviewQuestionList.map((interviewQuestion) =>*/}
-          {/*<InterviewQuestion interview={interviewQuestion}*/}
-          {/*/>*/}
-        {/*)}*/}
       </div>
     )
   }
@@ -97,27 +90,68 @@ class InterviewQuestionList extends React.Component {
 // TODO deleteQuestion
 
 const InterviewQuestion = (props) => {
+  //handleRemove
+
   //console.log('props', props);
-  let header = '('+props.interview.genre + ') ' + props.interview.title + ' - ' + props.interview.timeToAnswer + 'mins to answer ' + props.interview._id;
+  let header = '('+props.interview.genre + ') ' + props.interview.title + ' - ' + props.interview.timeToAnswer + 'mins to answer';
+  let btnId = 'btn-'+props.interview._id;
   return (
-  <div className="panel panel-primary">
-    <Accordion>
-      <Panel header={header} bsStyle="primary" eventKey={props.interview._id}>
+    <div className="panel panel-primary">
+      <div className="panel-heading clearfix" key={props.interview._id}>
+        <h4 className="panel-title pull-left">{header}</h4>
+        <div className="input-group-btn">
+          <button className="btn btn-primary pull-right glyphicon glyphicon-remove"
+                  onClick={props.removeQuestion}
+                  value={props.interview._id}
+          >
+
+          </button>
+          <button id={btnId} className="btn btn-primary pull-right glyphicon glyphicon-triangle-bottom"
+                  onClick={() => {
+                    //console.log(props.interview._id);
+                    $('#'+props.interview._id).toggleClass("in")
+                    $('#btn-'+props.interview._id).toggleClass("glyphicon-triangle-bottom glyphicon-triangle-top")
+                  }}>
+
+          </button>
+        </div>
+      </div>
+      <div id={props.interview._id} className="panel-body collapse">
         <Panel header="Question" bsStyle="danger">
           <p>{props.interview.questionText}</p>
         </Panel>
         <Panel header="Answer" bsStyle="success">
           <p>{props.interview.answerText}</p>
         </Panel>
-      </Panel>
-    </Accordion>
-  </div>
+      </div>
+    </div>
   )
 }
 
+
+// const InterviewQuestion = (props) => {
+//   //console.log('props.interview._id', props.interview._id);
+//   let header = '('+props.interview.genre + ') ' + props.interview.title + ' - ' + props.interview.timeToAnswer + ' mins to answer ...or else';
+//   return (
+//   <div className="panel panel-primary">
+//     <Accordion>
+//       <Panel header={header} bsStyle="primary" eventKey={props.interview._id}>
+//         <Panel header="Question" bsStyle="danger">
+//           <p>{props.interview.questionText}</p>
+//         </Panel>
+//         <Panel header="Answer" bsStyle="success">
+//           <p>{props.interview.answerText}</p>
+//         </Panel>
+//       </Panel>
+//     </Accordion>
+//   </div>
+//   )
+// }
+
 InterviewQuestionList.propTypes = {
   status: React.PropTypes.string.isRequired,
-  interviewQuestionList: React.PropTypes.array.isRequired
+  interviewQuestionList: React.PropTypes.array.isRequired,
+  removeQuestion: React.PropTypes.func.isRequired
 };
 
 export default InterviewQuestionList
